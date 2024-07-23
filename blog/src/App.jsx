31e6bcfb3f889df3de3bb09801e06c453afc8744
Vaanmathi2004 <vaanmathi.21ee@kct.ac.in>
2024-07-23 23:24:21 +0530

    create page and route

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import Home from "./pages/Home";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>APP</h1>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home/>}/> 
+    <Route path="/about" element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
