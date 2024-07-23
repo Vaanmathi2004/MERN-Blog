082ac216be05e71f35868461b075b344ec9fbdd5
Vaanmathi2004 <vaanmathi.21ee@kct.ac.in>
2024-07-23 23:40:24 +0530

    Commit pages and routes

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,5 +1,9 @@
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
-import React from "react";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
@@ -8,6 +12,10 @@ export default function App() {
     <Routes>
     <Route path="/" element={<Home/>}/> 
     <Route path="/about" element={<About/>}/>
+    <Route path="/dashboard" element={<Dashboard/>}/>
+    <Route path="/projects" element={<Projects/>}/>
+    <Route path="/sign-in" element={<SignIn/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
     </Routes>
     </BrowserRouter>
   )
